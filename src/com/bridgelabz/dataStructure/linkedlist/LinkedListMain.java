c9639dd2dc17b80893d3fc645d76374ed7aa5160
rajivjajo <rajivjajodia82552@gmail.com>
2023-01-31 02:27:03 +0530

    Adding elements in a linked list at head node

--- src/com/bridgelabz/dataStructure/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/dataStructure/linkedlist/LinkedListMain.java
@@ -3,9 +3,9 @@ package com.bridgelabz.dataStructure.linkedlist;
 public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList =  new LinkedList<>();
-        linkedList.push(56);
-        linkedList.push(30);
         linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
 
         linkedList.display();
     }
