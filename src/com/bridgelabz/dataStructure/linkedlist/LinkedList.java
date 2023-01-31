f110d6b2f66c106e79e989fc618b35a715dbfa4e
rajivjajo <rajivjajodia82552@gmail.com>
2023-01-31 19:26:38 +0530

    Refactored:insert() method to insert elements

--- src/com/bridgelabz/dataStructure/linkedlist/LinkedList.java
+++ src/com/bridgelabz/dataStructure/linkedlist/LinkedList.java
@@ -3,7 +3,7 @@ package com.bridgelabz.dataStructure.linkedlist;
 public class LinkedList <T>{
     Node<T> head;
     Node<T> tail;
-    public void add(T data){
+    public void push(T data){
         Node<T> newNode = new Node(data);
         if ( head == null ){
             head = newNode;
@@ -13,23 +13,10 @@ public class LinkedList <T>{
             head = newNode;
         }
     }
-    public void insert(T data) {
-        Node<T> newNode = new Node<>(data);
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-        Node<T> current = head;
-        while (current.next != null) {
-            current = current.next;
-        }
-        current.next = newNode;
-    }
-
     public void display(){
         Node<T> temp = head;
         while ( temp != null ){
-            System.out.print(temp.data+"->");
+            System.out.println(temp.data+"->");
             temp = temp.next;
         }
         System.out.println();
--- src/com/bridgelabz/dataStructure/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/dataStructure/linkedlist/LinkedListMain.java
@@ -3,14 +3,10 @@ package com.bridgelabz.dataStructure.linkedlist;
 public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList =  new LinkedList<>();
-        linkedList.add(70);
-        linkedList.add(30);
-        linkedList.add(56);
-        linkedList.display();
-        LinkedList<Integer> linkedList1 = new LinkedList<>();
-        linkedList.insert(40);
-        linkedList.display();
+        linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
 
-
+        linkedList.display();
     }
 }
