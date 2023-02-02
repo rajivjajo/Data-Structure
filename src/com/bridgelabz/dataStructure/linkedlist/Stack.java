48e17e70703414371f15e898d03582c5befe14fb
rajivjajo <rajivjajodia82552@gmail.com>
2023-02-03 01:27:55 +0530

    Peak and pop from Stack!

--- src/com/bridgelabz/dataStructure/linkedlist/Stack.java
+++ src/com/bridgelabz/dataStructure/linkedlist/Stack.java
@@ -9,6 +9,25 @@ public class Stack<T> {
     public void push(T data){
         list.push(data);
     }
+    public void peak(){
+
+        System.out.println("Popped Element : "+list.head.data);
+    }
+
+    public void pop(){
+        list.pop();
+    }
+
+    public void popPeakTillEmpty() {
+        while(list.head!=null) {
+            peak();
+            pop();
+            display();
+            System.out.print("\n");
+        }
+        System.out.println("Stack is now Empty");
+    }
+
 
     public void display() {
         list.display();
